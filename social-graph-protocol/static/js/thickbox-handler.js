824c82b6372b7728205b4f229414cab53a63e140
bizzyLabs <bizzyLabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-30 23:14:10 +0000

    Update Plugin for hotfix
    git-svn-id: https://plugins.svn.wordpress.org/social-graph-protocol/tags/2.0.3@720598 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ social-graph-protocol/static/js/thickbox-handler.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+	jQuery( "#imgup" ).click(function() {
+		tb_show( "", "media-upload.php?type=image&amp;TB_iframe=true" );
+		return false;
+	});
+
+	window.send_to_editor = function( html ) {
+		imgurl = jQuery( "img", html ).attr( "src" );
+		jQuery( "#image" ).val( imgurl );
+		tb_remove();
+	}
+});
